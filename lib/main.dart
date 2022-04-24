492ec9c0ee65744726a79ff0d340741a9b7ebfc3
Apple <apple@Apples-MacBook-Pro.local>
2022-04-24 10:24:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,315 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:health/health.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+enum AppState {
+  DATA_NOT_FETCHED,
+  FETCHING_DATA,
+  DATA_READY,
+  NO_DATA,
+  AUTH_NOT_GRANTED,
+  DATA_ADDED,
+  DATA_NOT_ADDED,
+  STEPS_READY,
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<HealthDataPoint> _healthDataList = [];
+  AppState _state = AppState.DATA_NOT_FETCHED;
+  int _nofSteps = 10;
+  double _mgdl = 10.0;
+
+  // create a HealthFactory for use in the app
+  HealthFactory health = HealthFactory();
+
+  /// Fetch data points from the health plugin and show them in the app.
+  Future fetchData() async {
+    setState(() => _state = AppState.FETCHING_DATA);
+
+    // define the types to get
+    final types = [
+      HealthDataType.STEPS,
+      HealthDataType.WEIGHT,
+      HealthDataType.HEIGHT,
+      HealthDataType.BLOOD_GLUCOSE,
+      // Uncomment this line on iOS - only available on iOS
+      // HealthDataType.DISTANCE_WALKING_RUNNING,
+    ];
+
+    // with coresponsing permissions
+    final permissions = [
+      HealthDataAccess.READ,
+      HealthDataAccess.READ,
+      HealthDataAccess.READ,
+      HealthDataAccess.READ,
+    ];
+
+    // get data within the last 24 hours
+    final now = DateTime.now();
+    final yesterday = now.subtract(const Duration(days: 1));
+
+    // requesting access to the data types before reading them
+    // note that strictly speaking, the [permissions] are not
+    // needed, since we only want READ access.
+    bool requested =
+        await health.requestAuthorization(types, permissions: permissions);
+
+    if (requested) {
+      try {
+        // fetch health data
+        List<HealthDataPoint> healthData =
+            await health.getHealthDataFromTypes(yesterday, now, types);
+
+        // save all the new data points (only the first 100)
+        _healthDataList.addAll((healthData.length < 100)
+            ? healthData
+            : healthData.sublist(0, 100));
+      } catch (error) {
+        print("Exception in getHealthDataFromTypes: $error");
+      }
+
+      // filter out duplicates
+      _healthDataList = HealthFactory.removeDuplicates(_healthDataList);
+
+      // print the results
+      _healthDataList.forEach((x) => print(x));
+
+      // update the UI to display the results
+      setState(() {
+        _state =
+            _healthDataList.isEmpty ? AppState.NO_DATA : AppState.DATA_READY;
+      });
+    } else {
+      print("Authorization not granted");
+      setState(() => _state = AppState.DATA_NOT_FETCHED);
+    }
+  }
+
+  /// Add some random health data.
+  Future addData() async {
+    final now = DateTime.now();
+    final earlier = now.subtract(const Duration(minutes: 5));
+
+    _nofSteps = Random().nextInt(10);
+    final types = [HealthDataType.STEPS, HealthDataType.BLOOD_GLUCOSE];
+    final rights = [HealthDataAccess.WRITE, HealthDataAccess.WRITE];
+    final permissions = [
+      HealthDataAccess.READ_WRITE,
+      HealthDataAccess.READ_WRITE
+    ];
+    bool? hasPermissions =
+        await HealthFactory.hasPermissions(types, permissions: rights);
+    if (hasPermissions == false) {
+      await health.requestAuthorization(types, permissions: permissions);
+    }
+
+    _mgdl = Random().nextInt(10) * 1.0;
+    bool success = await health.writeHealthData(
+        _nofSteps.toDouble(), HealthDataType.STEPS, earlier, now);
+
+    if (success) {
+      success = await health.writeHealthData(
+          _mgdl, HealthDataType.BLOOD_GLUCOSE, now, now);
+    }
+
+    setState(() {
+      _state = success ? AppState.DATA_ADDED : AppState.DATA_NOT_ADDED;
+    });
+  }
+
+  /// Fetch steps from the health plugin and show them in the app.
+  Future fetchStepData() async {
+    int? steps;
+
+    // get steps for today (i.e., since midnight)
+    final now = DateTime.now();
+    final midnight = DateTime(now.year, now.month, now.day);
+
+    bool requested = await health.requestAuthorization([HealthDataType.STEPS]);
+
+    if (requested) {
+      try {
+        steps = await health.getTotalStepsInInterval(midnight, now);
+      } catch (error) {
+        print("Caught exception in getTotalStepsInInterval: $error");
+      }
+
+      print('Total number of steps: $steps');
+
+      setState(() {
+        _nofSteps = (steps == null) ? 0 : steps;
+        _state = (steps == null) ? AppState.NO_DATA : AppState.STEPS_READY;
+      });
+    } else {
+      print("Authorization not granted");
+      setState(() => _state = AppState.DATA_NOT_FETCHED);
+    }
+  }
+
+  Widget _contentFetchingData() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+            padding: const EdgeInsets.all(20),
+            child: const CircularProgressIndicator(
+              strokeWidth: 10,
+            )),
+        const Text('Fetching data...')
+      ],
+    );
+  }
+
+  Widget _contentDataReady() {
+    return ListView.builder(
+        itemCount: _healthDataList.length,
+        itemBuilder: (_, index) {
+          HealthDataPoint p = _healthDataList[index];
+          return ListTile(
+            title: Text("${p.typeString}: ${p.value}"),
+            trailing: Text('${p.unitString}'),
+            subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+          );
+        });
+  }
+
+  Widget _contentNoData() {
+    return const Text('No Data to show');
+  }
+
+  Widget _contentNotFetched() {
+    return Column(
+      children: [
+        const Text('Press the download button to fetch data.'),
+        const Text('Press the plus button to insert some random data.'),
+        const Text('Press the walking button to get total step count.'),
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  Widget _authorizationNotGranted() {
+    return const Text('Authorization not given. '
+        'For Android please check your OAUTH2 client ID is correct in Google Developer Console. '
+        'For iOS check your permissions in Apple Health.');
+  }
+
+  Widget _dataAdded() {
+    return Text('$_nofSteps steps and $_mgdl mgdl are inserted successfully!');
+  }
+
+  Widget _stepsFetched() {
+    return Text('Total number of steps: $_nofSteps');
+  }
+
+  Widget _dataNotAdded() {
+    return const Text('Failed to add data');
+  }
+
+  Widget _content() {
+    if (_state == AppState.DATA_READY) {
+      return _contentDataReady();
+    } else if (_state == AppState.NO_DATA) {
+      return _contentNoData();
+    } else if (_state == AppState.FETCHING_DATA) {
+      return _contentFetchingData();
+    } else if (_state == AppState.AUTH_NOT_GRANTED) {
+      return _authorizationNotGranted();
+    } else if (_state == AppState.DATA_ADDED) {
+      return _dataAdded();
+    } else if (_state == AppState.STEPS_READY) {
+      return _stepsFetched();
+    } else if (_state == AppState.DATA_NOT_ADDED) {
+      return _dataNotAdded();
+    }
+
+    return _contentNotFetched();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Health Example'),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.file_download),
+                onPressed: () {
+                  fetchData();
+                },
+              ),
+              IconButton(
+                onPressed: () {
+                  addData();
+                },
+                icon: const Icon(Icons.add),
+              ),
+              IconButton(
+                onPressed: () {
+                  fetchStepData();
+                },
+                icon: const Icon(Icons.nordic_walking),
+              )
+            ],
+          ),
+          body: Center(
+            child: _content(),
+          )),
+    );
+  }
+}
